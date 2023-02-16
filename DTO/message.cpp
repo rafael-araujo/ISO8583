a5d0ca725955b67dc4075b499453e0b682e4e8b8
rafael.araujo_ntconsult@prestador.globo <rafael.araujo_ntconsult@prestador.globo>
2023-02-16 07:14:37 -0300

    first commit

---
+++ DTO/message.cpp
@@ -0,0 +1,43 @@
+#include "message.h"
+#include <string>
+
+class message {
+
+    void out(){
+        cout << "created object" << endl;
+    }
+
+    message(){}    
+
+    // Message(string mti, string primaryBitmap, string secundaryBitmap){
+
+    //     Message::mti = mti;
+    //     Message::primaryBitmap = primaryBitmap;
+    //     Message::secundaryBitmap = secundaryBitmap;
+
+    // }
+
+    // string getMti(){
+
+    // }
+
+    // string setMti(){
+
+    // }
+
+    // string getPrimaryBitmap(){
+
+    // }
+
+    // string setPrimaryBitmap(){
+
+    // }
+
+    // string getSecundaryBitmap(){
+
+    // }
+
+    // string setSecundaryBitmap(){
+
+    // }
+};
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Message.h"
+
+using namespace std;
+
+int main(int argc, char * argv[]) {
+
+    Message msg1;
+
+    msg1::out();
+}
